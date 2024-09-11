c9fb172ca07d3e94385997748a119e4504562e81
Faraz Ahmed <143342902+Faraz-18@users.noreply.github.com>
2024-09-11 02:34:12 -0700

    Create Question6.c

---
+++ Task3/Question6.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+float X1=5.0, Y1=4.0;
+float X2=3.0, Y2=2.0;
+float slope;
+
+slope = (Y2-Y1)/(X2-X1);
+ printf("The slope of this line is %.3f",slope);
+
+    return 0;
+}
