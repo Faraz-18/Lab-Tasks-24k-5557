3009c6d1f4e0079b775050da2a3e57738e8b9e18
Faraz Ahmed <143342902+Faraz-18@users.noreply.github.com>
2024-10-30 06:00:52 -0700

    Update Question5.c

--- Task7/Question5.c
+++ Task7/Question5.c
@@ -1,28 +1,23 @@
-# include <stdio.h>
-int main (void){
-    int flag =1;
-    int len;
-    int index=0;
-    char string[100];
-    char digit;
-    printf("Enter a palindrome:");
-    scanf("%s",string);
-    digit = string[index];
-    while (digit != '\0'){
-        index++;
-        digit = string[index];
-    }
-
-    for (int i=0; i<(index/2);i++){
-        
-        if (string[i]!= string[(index-1)-i]){
-            flag =-1;
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int length, isPalindrome=1;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    length = strlen(str);
+    for (int i = 0; i < length / 2; i++)
+    {
+        if (str[i] != str[length - i - 1])
+        {
+            isPalindrome = 0;
         }
     }
-    if (flag ==1){
-        printf("Yes it is a Palindrome");
-    }
-    else{
-        printf("No your lying. it is not a Palindrome");
-    }
+    if (isPalindrome)
+        printf("It is palindrome");
+    else
+        printf("Not paindrome");
+
+    return 0;
 }
