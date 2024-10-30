91940c43f1a589c494de3475f673bce36fe5e19a
Faraz Ahmed <143342902+Faraz-18@users.noreply.github.com>
2024-10-30 03:48:47 -0700

    Update Question1.c

--- Task7/Question1.c
+++ Task7/Question1.c
@@ -1,28 +1,25 @@
-# include <stdio.h>
-int main(void){
-    int arr[100];
-    int num;
+#include <stdio.h>
+int main()
+{
+    int arr[6];
     int temp;
-    printf("how many number to enter:");
-    scanf("%d",&num);
-    for ( int  i =0;i<num;i++){
-        printf("Enter number:");
-        scanf("%d",&arr[i]);
+    printf("Enter Numbers in array\n");
+    for (int i = 0; i < 6; i++)
+    {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    temp = arr[0];
+    arr[0] = arr[5];
+    arr[5] = temp;
+
+    for (int i = 0; i < 6; i++)
+    {
+
+        printf("%d", arr[i]);
         
     }
-    for (int i =(num-1);i>=0;i--){
-        if (i == (num-1)){
-            temp = arr[i];
-            
-        }
-        
-            arr[i] = arr[i-1];
-        
-            
-        }
-        arr[0]= temp;
-        
-    for (int i =0; i <num;i++){
-        printf("%d\n",arr[i]);
-    }
+    printf("\n");
+    return 0;
 }
