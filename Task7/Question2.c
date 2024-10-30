1ae018a253bc8767dfb01c711cff4f67316fd32e
Faraz Ahmed <143342902+Faraz-18@users.noreply.github.com>
2024-10-30 04:36:54 -0700

    Update Question2.c

--- Task7/Question2.c
+++ Task7/Question2.c
@@ -1,34 +1,20 @@
-# include <stdio.h>
-int main(void){
-    char string[100];
-    int counta=0,countb=0,countc=0,counte=0,countf=0;
-    printf("Enter a  name:");
-    scanf("%s",string);
-    
-    for(int i =0;i<sizeof(string);i++){ 
-        switch(string[i]){
-            case 'a':
-                counta++;
-                break;
-            case 'e':
-                countb++;
-                break;
-            case 'i':
-                countc++;
-                break;
-            case 'o':
-                counte++;
-                break;
-            case 'u':
-                countf++;
-                break;
-   
-        } 
-        
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char str[100];
+    int count = 0;
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; i < strlen(str); i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+        {
+            count++;
         }
-    printf("a = %d ",counta);
-    printf("e = %d ",countb);
-    printf("i = %d ",countc);
-    printf("o = %d ",counte);
-    printf("u = %d ",countf);
+    }
+    printf("Numbers of Vowels are %d", count);
+    return 0;
 }
